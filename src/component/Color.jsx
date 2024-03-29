93a9526aaaa560dfb653d8cd673c7ac8889674e1
Gaurav <gauravsaini86400@gmail.com>
2024-03-29 21:50:01 +0530

    color changer

---
+++ src/component/Color.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export class Color extends React.Component{
+    
+    render() {
+        let Style={
+            backgroundColor:this.props.hexCode
+        }
+        return(
+            <div className="colorcode">
+                <div className="color" style={Style}>                  
+                  <p>{this.props.hexCode}</p>
+                </div>
+                
+            </div>
+        )
+    }
+}
---
+++ src/component/Myapp.jsx
@@ -0,0 +1,40 @@
+import React  from "react";
+import { Color } from "./Color";
+
+export class Myapp extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            colorNum:20,
+            colors:[]
+        };
+        for(let i=0;i<this.state.colorNum;i++){
+                this.state.colors.push({hexCode: this.generateColor()})
+        }
+    }
+    // generateColor
+    generateColor(){
+        return'#'+ Math.random().toString(16).substr(-6);
+    }
+    updatecolor(index){
+        let colors = this.state.colors.slice();
+        const currentColor= this.generateColor();
+        colors[index].hexCode=currentColor;
+        this.setState({
+            colors:colors
+        })
+    }
+
+    render(){
+        return(
+            <div className="colorContainer">
+                {this.state.colors.map((color,index)=>
+                <Color key={`color-${index}`} index={index}
+                update ={this.updatecolor.bind(this)}
+                    hexCode={color.hexCode}
+                />
+              )}
+            </div>
+        )
+    }
+}
